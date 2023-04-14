2eca270c7e6c93fa3ef67b9b66748808dde50573
Kichelle <74385536+kichelle-cc@users.noreply.github.com>
2023-04-14 14:26:13 +0100

    Update home.py

--- home.py
+++ home.py
@@ -38,6 +38,7 @@ mapping = {'0_doc-topics-pct.json':{
 }}
 
 st.container()
+st.write(os.listdir(prefix+path))
 for fig in os.listdir(prefix+path):
     st.divider()
     st.markdown(f"<h2 style='text-align: center; color: grey;'>{mapping[fig]['title']}</h1>", unsafe_allow_html=True)
