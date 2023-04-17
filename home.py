cd46e946ac347c83919576345f70916765b99768
Kichelle <74385536+kichelle-cc@users.noreply.github.com>
2023-04-17 15:57:41 +0100

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ import os
 import openai
 from PIL import Image
 import streamlit as st
-openai.api_key = st.secrets["OPEN_API_KEY"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 from llama_index import LLMPredictor, PromptHelper, GPTSimpleVectorIndex
 from llama_index.logger import LlamaLogger
 from langchain import OpenAI
